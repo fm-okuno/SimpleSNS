b1d8a96510fc6c37a9b55474c5e106f44ee7d036
gohta <okuno@freemake.co.jp>
2020-04-07 10:15:53 +0900

    ViewController.swift 　・UIImageViewを使って画像を表示 　・toolbarを配置し、その上にActionボタンを設置。Actionボタンを押下するとモーダブルビューが表示されるようにした 　・モーダブルビューを表示後に、デバッグ欄に「表示完了」と出力
    Main.storyboard
    　・toolbar、Actionボタン、ImageViewを配置し、　ViewController.swiftと接続

--- SimpleSNS/SimpleSNS/ViewController.swift
+++ SimpleSNS/SimpleSNS/ViewController.swift
@@ -10,15 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    override public func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-    @IBOutlet weak var imageView: UIImageView!
-    @IBAction func showActivityView(_ sender: UIBarButtonItem) {
-        let controller = UIActivityViewController(activityItems: [imageView.image!], applicationActivities: nil)
-        self.present(controller, animated: true, completion: nil)
+    @IBOutlet private weak var imageView: UIImageView! //画像を表示するため、UIImageViewと接続
+    @IBAction private func actionShowActivityView(_ sender: UIBarButtonItem) { //toolbar上に配置したActionボタンと接続
+        
+        let controller = UIActivityViewController(activityItems: [imageView.image!], applicationActivities: nil) //UIActivityViewのインスタンスを定数controller内に格納し、引数にはimageViewの画像を指定。
+        
+        self.present(controller, animated: true, completion: {print("表示完了")}) //モーダルビューコントローラにcontrollerを表示し、表示が完了したらデバッグ欄に「表示完了」と出力。
+        
     }
-    
 }
